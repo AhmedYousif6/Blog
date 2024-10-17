98dada40ea41a977c1f1d323124ff8dbbcfe5060
AhmedYousif6 <83282254+AhmedYousif6@users.noreply.github.com>
2024-10-17 18:38:19 +0000

    add like functionality to comment

--- client/src/components/CommentSection.jsx
+++ client/src/components/CommentSection.jsx
@@ -1,5 +1,5 @@
 import { useSelector } from 'react-redux';
-import { Link } from 'react-router-dom';
+import { Link , useNavigate} from 'react-router-dom';
 import { Alert, Button, Textarea } from 'flowbite-react';
 import { useEffect, useState } from 'react';
 import Comment from './Comment';
@@ -9,6 +9,8 @@ export default function CommentSection({ postId }) {
     const [comment, setComment] = useState('');
     const [commentError, setCommentError] = useState(null);
     const [comments, setComments] = useState([]);
+    const navigate = useNavigate();
+
     const handleSubmit = async (e) => {
         e.preventDefault();
         if (comment.length > 200) {
@@ -47,9 +49,33 @@ export default function CommentSection({ postId }) {
         getComments();
     }, [postId]);
 
+    const handleLike = async (commentId) => {
+        try {
+            if (!currentUser) {
+                navigate('/sign-in');
+                return;
+            }
+            const res = await fetch(`/api/comment/likeComment/${commentId}`, {
+                method: 'PUT',
+            });
+            if (res.ok) {
+                const data = await res.json();
+                setComments(comments.map((comment) =>
+                    comment._id === commentId ? {
+                        ...comment,
+                        likes: data.likes,
+                        numberOfLikes: data.numberOfLikes,
+                    } : comment
+                    ));
+                }
+        } catch (error) {
+            console.log(error.message);
+        }
+    }
+
   return (
     <div className='max-w-2xl mx-auto w-full p-3'>
-        {currentUser ? (
+        { currentUser ? (
             <div className='flex items-center gap-1 my-5 text-gray-500 text-sm'>
                 <p>Signed in as:</p>
                 <img className='w-5 h-5 rounded-full object-cover'
@@ -66,7 +92,7 @@ export default function CommentSection({ postId }) {
                 </Link>
             </div>
         )}
-        {currentUser && (
+        { currentUser && (
             <form onSubmit={handleSubmit} className='border border-teal-500 rounded-md p-3'>
                 <Textarea placeholder='Add a comment...'
                     rows='3'
@@ -95,7 +121,7 @@ export default function CommentSection({ postId }) {
                 </div>
                 {
                     comments.map((comment) => (
-                        <Comment key={comment._id} comment={comment}/>
+                        <Comment key={comment._id} comment={comment} onLike={handleLike}/>
                     ))
                 }
             </>
