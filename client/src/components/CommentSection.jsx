b93eb32b3d60bd123e58d8ecd46e8db365c59037
AhmedYousif6 <83282254+AhmedYousif6@users.noreply.github.com>
2024-10-17 16:03:10 +0000

    show the comments of a post

--- client/src/components/CommentSection.jsx
+++ client/src/components/CommentSection.jsx
@@ -1,13 +1,14 @@
 import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { Alert, Button, Textarea } from 'flowbite-react';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import Comment from './Comment';
 
-export default function CommentSection({postId}) {
+export default function CommentSection({ postId }) {
     const { currentUser } = useSelector((state) => state.user);
     const [comment, setComment] = useState('');
     const [commentError, setCommentError] = useState(null);
-
+    const [comments, setComments] = useState([]);
     const handleSubmit = async (e) => {
         e.preventDefault();
         if (comment.length > 200) {
@@ -19,16 +20,32 @@ export default function CommentSection({postId}) {
                 headers: { 'Content-Type': 'application/json' },
                 body: JSON.stringify({ content: comment, postId, userId: currentUser._id }),
             });
-            const data = res.json();
+            const data = await res.json();
             if (res.ok) {
                 setComment('');
                 setCommentError(null);
+                setComments([data, ...comments]);
             }
         } catch (error) {
             setCommentError(error.message);
         }
         
     };
+
+    useEffect(() => {
+        const getComments = async () => {
+            try {
+                const res = await fetch(`/api/comment/getPostComments/${postId}`);
+                if (res.ok) {
+                    const data = await res.json();
+                    setComments(data);
+                }
+            } catch (error) {
+                console.log(error.message);
+            }
+        };
+        getComments();
+    }, [postId]);
 
   return (
     <div className='max-w-2xl mx-auto w-full p-3'>
@@ -67,6 +84,21 @@ export default function CommentSection({postId}) {
                     <Alert color='failure' className='mt-5'>{commentError}</Alert>
                 )}
             </form>
+        )}
+        { comments.length === 0 ? (
+            <p className='text-sm my-5'>No comment's yet.</p>
+        ) : (
+            <>
+                <div className='text:sm my-5 flex items-center gap-1'>
+                    <p>Comments</p>
+                    <div className='border border-gray-400 py-1 px-2 rounded-sm'><p>{comments.length}</p></div>
+                </div>
+                {
+                    comments.map((comment) => (
+                        <Comment key={comment._id} comment={comment}/>
+                    ))
+                }
+            </>
         )}
     </div>
   )
